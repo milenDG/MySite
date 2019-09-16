e2e587d713e575ca4637692f21de12d404c2d142
Milen Georgiev <mgeorgiev@technologica.com>
2019-09-16 17:52:50 +0300

    Home Controller added.

---
+++ MySite/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿namespace MySite.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class HomeController : Controller
+    {
+        [Route("/index.html")]
+        public IActionResult Index()
+        {
+            return this.Content(System.IO.File.ReadAllText("index.html"), "text/html");
+        }
+
+        [Route("/")]
+        public IActionResult Empty()
+        {
+            return this.Redirect("/index.html");
+        }
+    }
+}
