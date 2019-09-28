fdc2eecd8cfa6e4aa6e1f0e2ede9dc258026d935
Milen Georgiev <milendg99@gmail.com>
2019-09-28 22:41:01 +0100

    Map creating script loaded after domManipulations.js

--- MySite/wwwroot/js/domManipulations.js
+++ MySite/wwwroot/js/domManipulations.js
@@ -19,6 +19,7 @@ let htmlInInnerHtml = {
 $(document).ready(() => {
     setViewsAndNavs();
     appendAllData();
+    loadScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyB-wmIAGPIA5GYoWHHe8z4H22Gcy0p3rZE&callback=initMap', function () {});
 });
 
 function setViewsAndNavs() {
@@ -286,4 +287,24 @@ function initMap() {
         position: location,
         map: map
     });
+}
+
+function loadScript(url, callback) {
+    var script = document.createElement("script");
+    script.type = 'text/javascript';
+    if (script.readyState) {  // only required for IE <9
+        script.onreadystatechange = function () {
+            if (script.readyState === 'loaded' || script.readyState === 'complete') {
+                script.onreadystatechange = null;
+                callback();
+            }
+        };
+    } else {  //Others
+        script.onload = function () {
+            callback();
+        };
+    }
+
+    script.src = url;
+    document.getElementsByTagName('head')[0].appendChild(script);
 }
--- MySite/wwwroot/js/domManipulationsTranspiled.js
+++ MySite/wwwroot/js/domManipulationsTranspiled.js
@@ -18,6 +18,7 @@ var htmlInInnerHtml = {
 $(document).ready(function () {
     setViewsAndNavs();
     appendAllData();
+    loadScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyB-wmIAGPIA5GYoWHHe8z4H22Gcy0p3rZE&callback=initMap', function () { });
 });
 
 function setViewsAndNavs() {
@@ -290,4 +291,27 @@ function initMap() {
         position: location,
         map: map
     });
+}
+
+function loadScript(url, callback) {
+    var script = document.createElement("script");
+    script.type = 'text/javascript';
+
+    if (script.readyState) {
+        // only required for IE <9
+        script.onreadystatechange = function () {
+            if (script.readyState === 'loaded' || script.readyState === 'complete') {
+                script.onreadystatechange = null;
+                callback();
+            }
+        };
+    } else {
+        //Others
+        script.onload = function () {
+            callback();
+        };
+    }
+
+    script.src = url;
+    document.getElementsByTagName('head')[0].appendChild(script);
 }
