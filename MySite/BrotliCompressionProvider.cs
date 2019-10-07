e266cdef2d9d271685d308e819633aa3e07d6c42
Milen Georgiev <milendg99@gmail.com>
2019-10-08 00:47:37 +0100

    SVG deflating and caching added.

---
+++ MySite/BrotliCompressionProvider.cs
@@ -0,0 +1,16 @@
+﻿namespace MySite
+{
+    using Microsoft.AspNetCore.ResponseCompression;
+    using System.IO;
+    using System.IO.Compression;
+
+    public class BrotliCompressionProvider : ICompressionProvider
+    {
+
+        public string EncodingName => "br";
+
+        public bool SupportsFlush => true;
+
+        public Stream CreateStream(Stream outputStream) => new BrotliStream(outputStream, CompressionMode.Compress);
+    }
+}
--- MySite/Startup.cs
+++ MySite/Startup.cs
@@ -5,6 +5,9 @@
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.ResponseCompression;
+    using System.IO.Compression;
+    using System.Linq;
 
     public class Startup
     {
@@ -12,8 +15,28 @@
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            services.AddResponseCompression();
+            services.AddMvc(options =>
+            {
+                options.CacheProfiles.Add("Default30",
+                new CacheProfile()
+                {
+                    Duration = 86400
+                });
+            }).SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.Configure<GzipCompressionProviderOptions>(options => {
+                options.Level = CompressionLevel.Fastest;
+            });
+
+            services.AddResponseCompression(options => {
+                options.Providers.Add<BrotliCompressionProvider>();
+                options.EnableForHttps = true;
+                options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[] {
+                    "application/xhtml+xml",
+                    "application/atom+xml",
+                    "image/svg+xml",
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
