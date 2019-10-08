0c129d64049516709a53f3689f7aacc3059c505a
Milen Georgiev <milendg99@gmail.com>
2019-10-08 21:02:59 +0100

    Home Controller fixed.

---
+++ MySite/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿namespace MySite.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class HomeController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+
+        [Route("Home")]
+        [Route("Index")]
+        [Route("Home/Index")]
+        [Route("index.html")]
+        public IActionResult Slash()
+        {
+            return this.Redirect("/");
+        }
+    }
+}
