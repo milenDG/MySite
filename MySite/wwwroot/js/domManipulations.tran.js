64a94964820a5fcc9cf697926271453bd40907f2
Milen Georgiev <milendg99@gmail.com>
2020-07-18 21:29:08 +0300

    Final change fix cookies

--- MySite/wwwroot/js/domManipulations.tran.js
+++ MySite/wwwroot/js/domManipulations.tran.js
@@ -22,7 +22,6 @@ $(document).ready(function () {
         appendAllData();
     }
 
-    showCookieNotice();
     loadScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyB-wmIAGPIA5GYoWHHe8z4H22Gcy0p3rZE&maptype=satellite&callback=initMap', function () { });
 });
 
